eab5fd846bed777cf635ec394a9546ef883c37c1
Saurav Kumar <123621147+the-sauravkumar@users.noreply.github.com>
2026-07-28 00:19:05 +0530

    fixed bug

--- src/config/site.ts
+++ src/config/site.ts
@@ -41,3 +41,4 @@ export const siteConfig: SiteConfig = {
   ],
   githubUsername: "the-sauravkumar",
   resumeUrl: "https://drive.google.com/file/d/1fCBu5q9hIptCDZ_Drtm7beStAKyuc0-_/view?usp=sharing",
+};
